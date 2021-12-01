e0cc996409871c575d6b12e1c1ea741263522d50
KamalovRus <kamrus97@mail.ru>
2021-12-01 15:28:29 +0500

    Добавьте файлы проекта.

---
+++ ConsoleAppЗадача14/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat();
+            Dog dog1 = new Dog();
+            cat1.ShowInfo();
+            dog1.ShowInfo();
+        }
+    }
+    public abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal()
+        {
+            Name = default;
+        }
+        public abstract void Say();
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+    public class Cat : Animal
+    {
+        public override string Name { get; set; }
+
+        public Cat()
+        {
+            Name = "Котик";
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+    public class Dog : Animal
+    {
+        public override string Name { get; set; }
+
+        public Dog()
+        {
+            Name = "Песик";
+        }
+
+        public override void Say()
+        {
+            Console.Write("Гав");
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+            Console.ReadKey();
+        }
+    }
+}
